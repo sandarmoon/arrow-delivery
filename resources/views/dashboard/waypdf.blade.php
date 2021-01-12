b9262158f60e04058b3bc5a880e8dc2aab5848c1
thetpainghtut <thetpainghtut.bf@gmail.com>
2021-01-12 18:03:07 +0630

    edited way pdf

--- resources/views/dashboard/waypdf.blade.php
+++ resources/views/dashboard/waypdf.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <style type="text/css">
     .mmfont{
-      font-family: 'Zawgyi_One'
+      font-family: 'Myanmar3'
     }
 
     .table-warning{
