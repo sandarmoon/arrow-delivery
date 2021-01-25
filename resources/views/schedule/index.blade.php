2a5411d4105d601f272fbc9476d268082d1d673d
thetpainghtut <thetpainghtut.bf@gmail.com>
2021-01-25 14:38:10 +0630

    edit schedule index

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -165,12 +165,15 @@
                             <a href="{{route('checkitem',$row->id)}}" class="btn btn-sm btn-danger">{{ __("fail")}}</a>
                           @elseif($row->status==3)
                             <a href="#" class="btn btn-sm btn-secondary addamount" data-id="{{$row->schedule->id}}">{{ __("Add amount and qty")}}</a>
-                          @else
+                          @elseif($row->status == 0)
                             <button type="button" class="btn btn-sm btn-danger">{{ __("pending")}}</button>
                           @endif
 
                           @if($row->status != 4 || $row->schedule->quantity != count(($row->items)))
+                            @if($row->status != 5)
                             <a href="{{route('schedules.edit',$row->schedule->id)}}" class="btn btn-sm btn-warning">{{ __("Edit")}}</a>
+                            @endif
+                            
                             @if(count($row->items) == 0)
                             <form action="{{ route('schedules.destroy',$row->schedule->id) }}" method="POST" class="d-inline-block" onsubmit="return confirm('Are you sure?')">
                               @csrf
@@ -178,6 +181,10 @@
                               <button type="submit" class="btn btn-sm btn-danger">{{ __("Delete")}}</button>
                             </form>
                             @endif
+                          @endif
+
+                          @if($row->status == 5)
+                            <button type="button" class="btn btn-sm btn-success">Cleared</button>
                           @endif
                         </td>
                       </tr>
